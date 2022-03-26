5cc9e1d3113624e77ad49e0f23c8492fd61f4eda
Joseph W <3599@holbertonschool.com>
2022-03-25 20:42:47 -0500

    task 7 and pep8

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Task 4 - """
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import environ
@@ -11,10 +11,16 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 
 
+@app.errorhandler(404)
+def not_found(e):
+    return jsonify({"error": "Not found"})
+
+
 @app.teardown_appcontext
 def teardown(appcontext):
     """shuts down the database"""
     storage.close()
+
 
 if __name__ == '__main__':
     if environ.get('HBNB_API_HOST') is not None:
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,5 +1,8 @@
+#!/usr/bin/python3
+"""Returns json status OK"""
 from flask import jsonify
 from api.v1.views import app_views
+
 
 @app_views.route("/status", methods=['GET'])
 def status():
