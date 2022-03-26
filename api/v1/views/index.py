de8956f7b53b952457c3f713b5928bcdad18fd94
Kaylee Dao <kayleed2@gmail.com>
2022-03-26 10:43:51 -0500

    Returning object and count

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,22 @@
 """Returns json status OK"""
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
 
 
 @app_views.route("/status", methods=['GET'])
 def status():
     """Returns Status JSON"""
     return jsonify({'status': 'OK'})
+
+
+@app_views.route("/api/vi/stats", methods=['GET'])
+def count():
+    """Returns count of object"""
+    classes = ['amenities', 'cities', 'places', 'reviews',
+               'states', 'users']
+    dict = {}
+    for cls in classes:
+        num = storage.count(cls)
+        dict[cls] = num
+    return dict
